2069ea8a2b274971fcad44c2f96f81c3497f82c3
seolbin01 <seolbinpark01@gmail.com>
2026-02-02 16:35:07 +0900

    chore: 초기 환경 세팅

---
+++ src/main/java/com/benny/board_mate/BoardMateApplication.java
@@ -0,0 +1,13 @@
+package com.benny.board_mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benny/board_mate/BoardMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benny.board_mate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
